0b9f52db9c235172caf542a5f4ef3d3295a1cd15
Jesusthot <allwasdrox@gmail.com>
2025-04-16 23:30:48 +0530

    Bin Spawning
    It works almost as intended, The bin spawns on the floor but it sometimes spawns inside an object. Need to fix that.

---
+++ Assets/Scripts/DustbinSpawn.cs
@@ -0,0 +1,38 @@
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using Ionic.Zip;
+using Meta.XR.MRUtilityKit;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DustbinSpawn : MonoBehaviour
+{
+    public GameObject SpawnPrefab;
+    public float minDistanceEdge = 0.5f;
+    public MRUKAnchor.SceneLabels spawnLabels;
+    private Vector3 location;
+    private Vector3 ObjectChecker;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {   
+        //SpawnPrefab.SetActive(false);
+        SpawnDustbin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpawnDustbin()
+    {
+        MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+        room.GenerateRandomPositionOnSurface(MRUK.SurfaceType.FACING_UP,minDistanceEdge,new LabelFilter(spawnLabels), out Vector3 pos, out Vector3 norm);
+        ObjectChecker = new Vector3(pos.x,1,pos.z);
+        pos.y = 0;
+        location = pos;
+        Instantiate(SpawnPrefab,location,Quaternion.identity);
+    }
+
+}
